97c4ae4efde80617c0f4a9ea802f6c086212014f
Shadow Fox <cool.afkshnik@gmail.com>
2026-08-27 01:33:47 +0300

    fix(config): remove hardcoded login credentials from client-side
    - Remove VITE_PANEL_LOGIN and VITE_PANEL_PASSWORD from env config
    - Clean up index.html window.__ENV__ object
    - Update docker entrypoint to stop substituting removed env vars
    - Security: credentials should only come from backend auth endpoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-envsubst '${VITE_API_URL} ${VITE_PANEL_LOGIN} ${VITE_PANEL_PASSWORD}' \
+envsubst '${VITE_API_URL}' \
   < /usr/share/nginx/html/index.html \
   > /usr/share/nginx/html/index.html.tmp
 
